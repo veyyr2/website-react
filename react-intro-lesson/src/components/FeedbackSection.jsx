d2c0f120459d59adc877ddcc0e71aa4406c6fc8e
veyrr2 <gorokhov_d@iuca.kg>
2025-06-05 16:31:32 +0600

    modern form

--- react-intro-lesson/src/components/FeedbackSection.jsx
+++ react-intro-lesson/src/components/FeedbackSection.jsx
@@ -1,6 +1,14 @@
-import Button from "./Button" // компонент кнопка
+import Button from './Button' // компонент кнопка
+import { useState } from 'react'
 
 export default function FeedbackSection() {
+    const [name, setName] = useState('') // следить за изменением в форме ИМЯ
+    const [reason, setReason] = useState('notError') // следить за изменением в форме ПРИЧИНА
+
+    // функция для изменения значения в имени
+    function handlerNameChange(event) {
+        setName(event.target.value)
+    }
 
     return (
         <section>
@@ -8,17 +16,26 @@ export default function FeedbackSection() {
 
             <form action="">
                 <label htmlFor="name">Ваше имя:</label>
-                <input type="text" id="name" className="control" />
+                <input
+                    type="text"
+                    id="name"
+                    className="control"
+                    value={name}
+                    onChange={handlerNameChange}
+                />
 
                 <label htmlFor="reason">Причина</label>
-                <select id="reason" className="control">
+                <select
+                    id="reason"
+                    className="control"
+                    value={reason}
+                    onChange={(e) => setReason(e.target.value)}
+                >
                     <option value="error">Ошибка</option>
                     <option value="notError">НЕ ошибка</option>
                 </select>
 
-                <Button>
-                    Отправить форму
-                </Button>
+                <Button>Отправить форму</Button>
             </form>
         </section>
     )
