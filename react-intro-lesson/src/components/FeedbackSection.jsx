40fe3da7549b79440a72c05d9a02d99cc94bc9a9
veyrr2 <gorokhov_d@iuca.kg>
2025-06-05 15:59:59 +0600

    created form

--- react-intro-lesson/src/components/FeedbackSection.jsx
+++ react-intro-lesson/src/components/FeedbackSection.jsx
@@ -1,8 +1,25 @@
+import Button from "./Button" // компонент кнопка
+
 export default function FeedbackSection() {
 
     return (
         <section>
             <h3>Обратная связь</h3>
+
+            <form action="">
+                <label htmlFor="name">Ваше имя:</label>
+                <input type="text" id="name" className="control" />
+
+                <label htmlFor="reason">Причина</label>
+                <select id="reason" className="control">
+                    <option value="error">Ошибка</option>
+                    <option value="notError">НЕ ошибка</option>
+                </select>
+
+                <Button>
+                    Отправить форму
+                </Button>
+            </form>
         </section>
     )
 }
